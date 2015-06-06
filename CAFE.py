9357806fb02a6f21e9a5b47e2c474db3ce875966
WeeBeasties <franklc@ferris.edu>
2015-06-06 10:04:08 -0400

    Python template
    Set up the initial outline of the python script file with commented
    chunks for completion.

---
+++ CAFE.py
@@ -0,0 +1,94 @@
+#!/opt/local/bin/python
+
+###############################################################################
+# +-------------------------------------------------------------------------+ #
+# |                                                                         | #
+# |  CAFE - Python version 1                                                | #
+# |  Computer-Assisted Formative Evaluations                                | #
+# |                                                                         | #
+# |  This script was written to generate formative feedback documents in an | #
+# |  html file for my students following major exams. These documents can be| #
+# |  emailed to each student, uploaded to a course management system, or    | #
+# |  directly returned to the students. I hope that these detailed reports  | #
+# |  will provide an effective means to address potential study strategies, | #
+# |  test-taking skills. Ultimately, I hope to encourage metacognitive      | #
+# |  reflection and increase student involvement in their own learning.     | #
+# |                                                                         | #
+# |                THE ORGANIZATION OF THIS SCRIPT'S PROCESSES              | #
+# |             --------------------------------------------------------    | #
+# |             1) Data input from external sources                         | #
+# |             2) Calculations necessary for the reports                   | #
+# |             3) Drawing graphs and figures for the instructor report     | #
+# |             4) Generation of individual student feedback reports        | #
+# |             5) Generation of an instructor summary report               | #
+# |             6) Subroutines that are called for repetitive tasks         | #
+# |                                                                         | #
+# |  written by:   Dr. Clifton Franklund                                    | #
+# |                Professor of Microbiology                                | #
+# |                Department of Biological Sciences                        | #
+# |                Ferris State University                                  | #
+# |                January 23, 2007                                         | #
+# |                                                                         | #
+# |  last revised: June 6,                                                  | #
+# |                Beginning the switch to python                           | #
+# |                                                                         | #
+# +-------------------------------------------------------------------------+ #
+###############################################################################
+
+
+
+###############################################################################
+# +-------------------------------------------------------------------------+ #
+# | [-1-]     Data input from external sources                              | #
+# +-------------------------------------------------------------------------+ #
+###############################################################################
+
+
+
+
+###############################################################################
+# +-------------------------------------------------------------------------+ #
+# | [-2-]     Calculations concerning the class performance                 | #
+# +-------------------------------------------------------------------------+ #
+###############################################################################
+
+
+
+
+###############################################################################
+# +-------------------------------------------------------------------------+ #
+# | [-3-]     Drawing graphics and figures for the instructor report        | #
+# +-------------------------------------------------------------------------+ #
+###############################################################################
+
+
+
+
+###############################################################################
+# +-------------------------------------------------------------------------+ #
+# | [-4-]     Generation of individual student feedback reports             | #
+# |           The reports may be emailed to each student if desired         | #
+# +-------------------------------------------------------------------------+ #
+###############################################################################
+
+
+
+
+###############################################################################
+# +-------------------------------------------------------------------------+ #
+# | [-5-]     Generation of the instructor summary report                   | #
+# |           This document has links to each of the student reports        | #
+# +-------------------------------------------------------------------------+ #
+###############################################################################
+
+
+
+
+###############################################################################
+# +-------------------------------------------------------------------------+ #
+# | [-6-]     Subroutines that are called for repetitive tasks              | #
+# +-------------------------------------------------------------------------+ #
+###############################################################################
+
+
+
